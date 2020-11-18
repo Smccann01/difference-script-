440287a418a16da7d534ada6fd5dc10109348736
PupiLePoo <3212669+Smccann01@users.noreply.github.com>
2020-11-18 12:54:44 -0600

    Update diff.py

--- diff.py
+++ diff.py
@@ -1,17 +1,19 @@
-
 import difflib
 from tkinter.filedialog import askopenfilename
 import os
 os.path.dirname(os.path.abspath(__file__))
 
+
 file1 = askopenfilename()
 file2 = askopenfilename()
-     
+
 file1_lines = open (file1).readlines()
 file2_lines = open (file2).readlines()
 
 
 difference = difflib.HtmlDiff().make_file(file1_lines, file2_lines, file1, file2)
-difference_report = open('Steve.html' , 'w')
+difference_report = open('Juxtapose.html' , 'w')
 difference_report.write(difference)
+diff = difflib.ndiff(open(file1).readlines(),open(file2).readlines())
+#print ''.join(diff),
 difference_report.close
